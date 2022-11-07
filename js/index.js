7866ac9d7ca133ee4bdb1e0d7b09874c7bf0776d
kukli07 <84287747+kukli07@users.noreply.github.com>
2022-11-07 12:38:09 +0100

    Change MOTD API

--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
-const motd = document.getElementById("motd");
-const today = {
-  day: new Date().getDate(),
-  month: new Date().getMonth() + 1,
-};
+function fetchMOTD() {
+  const motd = document.getElementById("motd");
+  const today = {
+    day: new Date().getDate(),
+    month: new Date().getMonth() + 1,
+  };
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "99eced56f8msh3205130cf90635dp1180edjsnb8046a39cf1c",
+      "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
+    },
+  };
 
-fetch(`http://numbersapi.com/${today.month}/${today.day}/date`, {
-  method: "GET",
-})
-  .then((response) => response.text())
-  .then((data) => {
-    motd.innerText = data;
-  })
-  .catch((error) => {
-    console.log(error);
-    motd.innerText = "One Place to rule them all";
-  });
+  fetch(
+    `https://numbersapi.p.rapidapi.com/${today.month}/${today.day}/date?fragment=false&json=false`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      motd.innerText = data.text;
+    })
+    .catch((error) => {
+      console.log(error);
+      motd.innerText = "One Place to rule them all";
+    });
+}
+
+fetchMOTD();
